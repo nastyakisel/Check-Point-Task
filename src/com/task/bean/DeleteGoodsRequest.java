5436503dabca12262a81035c06b482739372e2e0
OSmol <Olga_Smolyakova@outlook.com>
2016-10-29 16:12:37 +0300

    Update DeleteGoodsRequest.java

--- src/com/task/bean/DeleteGoodsRequest.java
+++ src/com/task/bean/DeleteGoodsRequest.java
@@ -4,7 +4,7 @@ import com.task.bean.entity.Goods;
 import com.task.bean.entity.Velogoods;
 
 public class DeleteGoodsRequest extends Request{
-	Integer id;
+	Integer id;// кто украл атрибут доступа?
 
 	public Integer getId() {
 		return id;
